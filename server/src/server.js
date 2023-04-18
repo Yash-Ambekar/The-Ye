c64a416ea9459c74d9a3762c3dc03b5dd7deba5d
Yash-Ambekar <yashambekar03@gmail.com>
2023-04-18 17:21:54 +0530

    Ngrok done

--- server/src/server.js
+++ server/src/server.js
@@ -11,6 +11,60 @@ app.use(cors());
 app.use(express.json());
 app.use('/messages', messageRouter);
 
+app.get('/', (req, res)=>{
+    return res.send("Hello World");
+})
+app.get('/webhooks',  (req, res) => {
+    console.log(req);
+    if (
+        req.query['hub.mode'] == 'subscribe' &&
+        req.query['hub.verify_token'] == "theye"
+    ) {
+        res.send(req.query['hub.challenge']);
+    } else {
+        res.sendStatus(400);
+    }
+});
+
+app.post("/webhooks", (req, res) => {
+    // Parse the request body from the POST
+    let body = req.body;
+    // console.log(body);
+    // if(body){
+    //     res.sendStatus(200);
+    // }
+    // else{
+    //     res.sendStatus(400);
+    // }
+    // info on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
+    if (req.body.object) {
+      if (
+        req.body.entry &&
+        req.body.entry[0].changes &&
+        req.body.entry[0].changes[0] &&
+        req.body.entry[0].changes[0].value.messages &&
+        req.body.entry[0].changes[0].value.messages[0]
+      ) {
+  
+        // do your stuff here.....
+  
+        let phone_number_id =
+          req.body.entry[0].changes[0].value.metadata.phone_number_id;
+        let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
+        let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
+        console.log("message Details:", JSON.stringify({
+            phone_id: phone_number_id,
+            from_whom: from,
+            msg: msg_body
+        }))
+      }
+      res.sendStatus(200);
+    } else {
+      // Return a '404 Not Found' if event is not from a WhatsApp API
+      res.sendStatus(404);
+    }
+  });
+
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}`);
 })
